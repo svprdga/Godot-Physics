e2bb7912a46473a937b0e6aa24c28e7598027509
David Serrano <david@davidserrano.io>
2023-09-27 18:19:04 +0200

    Update project

--- spawn_point/spawn_point.gd
+++ spawn_point/spawn_point.gd
@@ -1,12 +1,21 @@
 extends Node3D
 
 const CUBE_SCENE = preload("res://cube/cube.tscn")
-const SPHERE_SCENE = preload("res://sphere/sphere.tscn")
+const TIMER_INTERVAL = 0.01
 
 signal instantiate_cube
 
+var _elapsed_time: float = 0.0
 
-func _on_timer_timeout():
+
+func _process(delta: float) -> void:
+	_elapsed_time += delta
+	if _elapsed_time >= TIMER_INTERVAL:
+		_elapsed_time = 0.0
+		_instantiate_cube()
+
+
+func _instantiate_cube():
 	var object = CUBE_SCENE.instantiate()
 	emit_signal("instantiate_cube")
 
